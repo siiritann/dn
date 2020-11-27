08d23d4c3686559a96f866099309837537a6cbdf
siiri <siiri.tann@gmail.com>
2020-11-27 19:30:12 +0200

    Initial commit

---
+++ src/main/java/com/example/datanor/DatanorApplication.java
@@ -0,0 +1,13 @@
+package com.example.datanor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatanorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatanorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datanor/DatanorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datanor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatanorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
