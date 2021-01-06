2541d8e60f6f5bb04487bf235b054963298b1bca
siiri <siiri.tann@gmail.com>
2021-01-06 16:31:31 +0200

    moved init() from controller to service

--- src/main/java/com/example/datanor/controller/CityController.java
+++ src/main/java/com/example/datanor/controller/CityController.java
@@ -3,14 +3,9 @@ package com.example.datanor.controller;
 import com.example.datanor.exception.ApplicationException;
 import com.example.datanor.model.City;
 import com.example.datanor.service.CityService;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.io.FileReader;
 import java.util.List;
 
 @CrossOrigin
@@ -20,31 +15,6 @@ public class CityController {
     @Autowired
     private CityService cityService;
 
-
-    @PostConstruct
-    public void init() {
-
-        if (cityService.countBaseCities() != 0) {
-            System.out.println(cityService.countBaseCities());
-        } else {
-            JSONParser parser = new JSONParser();
-            try {
-                Object objectArray = parser
-                        .parse(new FileReader("src/main/resources/static/city.list.json"));
-                JSONArray jsonArray = (JSONArray) objectArray;
-                for (Object object : jsonArray) {
-                    JSONObject jsonObject = (JSONObject) object;
-                    Long id = (Long) jsonObject.get("id");
-                    String name = (String) jsonObject.get("name");
-                    String country = (String) jsonObject.get("country");
-                    String state = (String) jsonObject.get("state");
-                    cityService.addCitiesToBase(id, name, country, state);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     @GetMapping("/cities")
     public List<City> getCityByName(@RequestParam("name") String name) {
--- src/main/java/com/example/datanor/service/CityService.java
+++ src/main/java/com/example/datanor/service/CityService.java
@@ -3,8 +3,14 @@ package com.example.datanor.service;
 import com.example.datanor.exception.ApplicationException;
 import com.example.datanor.model.City;
 import com.example.datanor.repository.CityRepository;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
 import java.util.List;
 
 @Service
@@ -12,6 +18,33 @@ public class CityService {
 
     @Autowired
     private CityRepository cityRepository;
+
+
+    // TODO refacto classloaderiga
+
+    @PostConstruct
+    public void init() {
+        if (countBaseCities() != 0) {
+            System.out.println(countBaseCities());
+        } else {
+            JSONParser parser = new JSONParser();
+            try {
+                Object objectArray = parser
+                        .parse(new FileReader("src/main/resources/static/city.list.json"));
+                JSONArray jsonArray = (JSONArray) objectArray;
+                for (Object object : jsonArray) {
+                    JSONObject jsonObject = (JSONObject) object;
+                    Long id = (Long) jsonObject.get("id");
+                    String name = (String) jsonObject.get("name");
+                    String country = (String) jsonObject.get("country");
+                    String state = (String) jsonObject.get("state");
+                    addCitiesToBase(id, name, country, state);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public void addCitiesToBase(long id, String name, String country, String state) {
         cityRepository.addCitiesToBase(id, name, country, state);
@@ -48,7 +81,6 @@ public class CityService {
             throw new ApplicationException("Something went wrong");
         }
     }
-
 
     public String getCityNameById(long id) {
         try {
