ac354850970b5620499e027d05d7a6bc1e3e61f5
siiritann <47256441+siiritann@users.noreply.github.com>
2021-01-13 14:19:10 +0200

    Added swagger (springfox) to the project; CityController.java updated with some examples (#4)

--- src/main/java/com/example/datanor/DatanorApplication.java
+++ src/main/java/com/example/datanor/DatanorApplication.java
@@ -2,14 +2,43 @@ package com.example.datanor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableScheduling
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @SpringBootApplication
+@EnableScheduling
+@EnableSwagger2
 public class DatanorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatanorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DatanorApplication.class, args);
+    }
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.datanor.controller"))
+                .paths(regex("/weather.*").or(regex("/cities.*"))) // to exclude testing urls
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Simple weather API")
+                .description("API for getting weather for your city")
+                .version("1.0")
+                .build();
+    }
 
 }
--- src/main/java/com/example/datanor/controller/CityController.java
+++ src/main/java/com/example/datanor/controller/CityController.java
@@ -3,12 +3,14 @@ package com.example.datanor.controller;
 import com.example.datanor.exception.ApplicationException;
 import com.example.datanor.model.City;
 import com.example.datanor.service.CityService;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin
 @RestController
+@Api(description = "Set of endpoints for searching cities, adding, viewing and deleting my cities.")
 public class CityController {
 
     private final CityService cityService;
@@ -24,14 +26,19 @@ public class CityController {
     }
 
 
+    @ApiOperation("Returns list of my cities.")
     @GetMapping("/cities/my")
     public List<City> getMyCities() {
         return cityService.getMyCities();
     }
 
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Incorrect city name")
+    })
     @PostMapping("cities/add")
-    public void addTrackedCity(@RequestParam("id") long id) {
+    public void addTrackedCity(@ApiParam("id of the city you want to add to your list")
+                               @RequestParam("id") long id) {
         if (id < 1) {
             throw new ApplicationException("Incorrect city name");
         } else {
@@ -39,6 +46,7 @@ public class CityController {
         }
     }
 
+    @ApiOperation(value = "Delete your city")
     @DeleteMapping("cities/delete")
     public String deleteCity(@RequestParam("id") long id) {
         return cityService.deleteCity(id);
