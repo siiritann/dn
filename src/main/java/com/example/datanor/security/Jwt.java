83644aa016835c5c00588ddd0f7cffb989c9594a
siiri <siiri.tann@gmail.com>
2021-02-06 12:05:04 +0200

    Jwt

---
+++ src/main/java/com/example/datanor/security/Jwt.java
@@ -0,0 +1,27 @@
+package com.example.datanor.security;
+
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+public class Jwt {
+
+    public String getBearerToken(Long intranetuserId, String username, boolean isAdmin) {
+        JwtBuilder builder = Jwts.builder()
+                .setExpiration(Date.from(ZonedDateTime.now().plusDays(7).toInstant()))
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(new Date())
+                .setIssuer("intranet")
+                .signWith(SignatureAlgorithm.HS256, "secret")
+                .claim("intranetuserId", intranetuserId)
+                .claim("username", username)
+                .claim("isAdmin", isAdmin);
+        String jwt = builder.compact();
+        return jwt;
+    }
+}
