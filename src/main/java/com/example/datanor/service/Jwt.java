4af2db68117962ece360687730e1198397eb6e02
Siiri <siiri.tann@tripledev.ee>
2021-02-26 18:36:58 +0200

    jwt update

--- src/main/java/com/example/datanor/service/Jwt.java
+++ src/main/java/com/example/datanor/service/Jwt.java
@@ -2,12 +2,17 @@ package com.example.datanor.service;
 
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.UUID;
 
+import static io.jsonwebtoken.io.Decoders.BASE64;
+
+@Service
 public class Jwt {
 
     public String getBearerToken(Long id, String username) {
@@ -16,9 +21,14 @@ public class Jwt {
                 .setId(UUID.randomUUID().toString())
                 .setIssuedAt(new Date())
                 .setIssuer("weatherAPI")
-                .signWith(SignatureAlgorithm.HS256, "jsonwebtokenforsiiriweatherapisecretformoresecuritythanpreviously")
+                .signWith(getSigningKey())
                 .claim("id", id)
                 .claim("username", username);
         return builder.compact();
+    }
+
+    public Key getSigningKey() {
+        byte[] keyBytes = BASE64.decode("jsonwebtokenforsiiriweatherapisecretformoresecuritythanpreviously");
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
